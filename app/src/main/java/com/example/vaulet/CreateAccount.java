589ed4c9c3784eb7e14a3fb2f93c49530eb8290f
rvgarg <raghav.garg175@gmail.com>
2019-01-29 01:06:23 +0530

    adding online storage

--- app/src/main/java/com/example/vaulet/CreateAccount.java
+++ app/src/main/java/com/example/vaulet/CreateAccount.java
@@ -2,16 +2,12 @@ package com.example.vaulet;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -31,10 +27,6 @@ public class CreateAccount extends AppCompatActivity {
         signup.setOnClickListener(v -> {
             String email = userid.getText().toString();
             String password = pass.getText().toString();
-            if (email.equals(null) || password.equals(null)) {
-                Toast.makeText(CreateAccount.this, "Either emailid or Password not entered",
-                        Toast.LENGTH_SHORT).show();
-            } else {
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(this, task -> {
                             if (task.isSuccessful()) {
@@ -51,20 +43,20 @@ public class CreateAccount extends AppCompatActivity {
 
                             // ...
                         });
-            }
         });
     }
 
-    public void updateUI(FirebaseUser user) {
+    private void updateUI(FirebaseUser user) {
         Intent intent = new Intent(CreateAccount.this, secondActivity.class);
         startActivity(intent);
     }
+
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser!=null)
-        updateUI(currentUser);
+        if (currentUser != null)
+            updateUI(currentUser);
     }
 }
--- app/src/main/java/com/example/vaulet/MainActivity.java
+++ app/src/main/java/com/example/vaulet/MainActivity.java
@@ -2,21 +2,17 @@ package com.example.vaulet;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    public FirebaseAuth mAuth;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -27,31 +23,28 @@ public class MainActivity extends AppCompatActivity {
         final Button signin = findViewById(R.id.signin), newid = findViewById(R.id.newid);
         final EditText emal = findViewById(R.id.email), pass = findViewById(R.id.password);
         signin.setOnClickListener(v -> {
-            String email=emal.getText().toString();
-            String password=pass.getText().toString();
+            String email = emal.getText().toString();
+            String password = pass.getText().toString();
             mAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d("TODO", "signInWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                updateUI(user);
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w("TODO", "signInWithEmail:failure", task.getException());
-                                Toast.makeText(MainActivity.this, "Authentication failed.",
-                                        Toast.LENGTH_SHORT).show();
-                                updateUI(null);
-                            }
-
-                            // ...
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TODO", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TODO", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
                         }
+
+                        // ...
                     });
         });
         newid.setOnClickListener(v -> {
-            Intent intent=new Intent(MainActivity.this,CreateAccount.class);
+            Intent intent = new Intent(MainActivity.this, CreateAccount.class);
             startActivity(intent);
 
         });
@@ -62,11 +55,12 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser!= null)
-        updateUI(currentUser);
+        if (currentUser != null)
+            updateUI(currentUser);
     }
-    public void updateUI(FirebaseUser user){
-        Intent intent=new Intent(MainActivity.this,secondActivity.class);
+
+    private void updateUI(FirebaseUser user) {
+        Intent intent = new Intent(MainActivity.this, secondActivity.class);
         startActivity(intent);
     }
 }
