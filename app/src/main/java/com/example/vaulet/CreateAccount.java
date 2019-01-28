a4a5f9db66c0d14f9c41980e99e3dad2e7ef9b63
rvgarg <raghav.garg175@gmail.com>
2019-01-29 00:59:25 +0530

    adding online storage

--- app/src/main/java/com/example/vaulet/CreateAccount.java
+++ app/src/main/java/com/example/vaulet/CreateAccount.java
@@ -36,24 +36,20 @@ public class CreateAccount extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             } else {
                 mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d("TODO", "createUserWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    updateUI(user);
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w("TODO", "createUserWithEmail:failure", task.getException());
-                                    Toast.makeText(CreateAccount.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                    updateUI(null);
-                                }
-
-                                // ...
+                        .addOnCompleteListener(this, task -> {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("TODO", "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("TODO", "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(CreateAccount.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
                             }
+
+                            // ...
                         });
             }
         });
--- app/src/main/java/com/example/vaulet/secondActivity.java
+++ app/src/main/java/com/example/vaulet/secondActivity.java
@@ -3,11 +3,15 @@ package com.example.vaulet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class secondActivity extends AppCompatActivity {
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+public class secondActivity extends AppCompatActivity {
+    private StorageReference mStorageRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
 }
